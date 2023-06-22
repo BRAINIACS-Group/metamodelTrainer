7d499c95923932df1445d780f2114473f22c4943
cferlay <ferlaycarl@gmail.com>
2023-06-22 13:20:06 +0200

    prepare MM improve loop small scale

--- src/main.py
+++ src/main.py
@@ -20,32 +20,33 @@ save_path = Path(cwd,"models")
 R = [(-20,20),(100,2000),(-20,20),(100,2000),(0,10000)]
 
 #Create an initial sampling of the parameter space (k points, Latin Hypercube method)
-k = 24
+k = 20
 S = LHCuSample(R,k)
 
 #Label them by running simulations, and keep in a variable the folder in which the results are saved
-res_path = label(S)
-
+#res_path = label(S)
+res_path = data_path
 #Load the results and format them for model training
 X_T,Y_T = load_FE(res_path)
 
 #Define model HyperParameters
 HP = HyperParameters(layers=[64,64],
                      loss='mae',
-                     dropout_rate=0.5,
+                     dropout_rate=0,
                      interpolation=1000)
 
 #Build model
-model = RecModel(X_T,Y_T,HP)
+#model = RecModel(X_T,Y_T,HP)
+model = MegaModel(X_T,Y_T,10,'RNN',HP)
 
 #train model
-model.train(n_epochs=100, verbose=1)
+model.train(n_epochs=10, verbose=1)
 
-model.save(Path(save_path,"model_AL_base"))
+model.save(Path(save_path,"megaModel_AL_base"))
 
 label_fn = lambda X: load_FE(label(X))
 
-for i in range(119):
+for i in range(2):
     model_bis = improve(model,label_fn,R,k=4)
     model_bis.save(Path(save_path,f"model_AL_improved_{str(i).zfill(3)}"))
     model = model_bis
@@ -57,6 +58,6 @@ HPf = HyperParameters(layers=[64,64],
                      dropout_rate=0,
                      interpolation=1000)
 
-final = model(X_T,Y_T,HPf)
+final = MegaModel(X_T,Y_T,10,'RNN',HPf)
 final.train(1000,1)
-final.save(Path(save_path,"model_AL_Final"))
+final.save(Path(save_path,"megaModel_AL_Final"))
