5dcf22bc161db0ab01d607c2edc86a56b6da8d52
Jan Hinrichsen <j.hinrichsen95@gmail.com>
2023-12-06 14:08:35 -0800

    added timing functions

--- metamodeltrainer/build_example_force_active.py
+++ metamodeltrainer/build_example_force_active.py
@@ -21,23 +21,25 @@ pyLabPath = parent_dir / "efiPyVlab/src"
 sys.path += [str(pyLabPath),
         str(Path(__file__).resolve().parents[1])]
 
-import pyVlab
+import time
 #STL modules
 from datetime import datetime
 from pathlib import Path
 
 #Local models
 
-from metamodeltrainer.explore_param_space import ParameterSpace, ExData, Sample, PDskSample  #Data structure & Sampling method
-from metamodeltrainer.models import HyperParameters, RecModel, improve, load_model           #Neural Network management
+from metamodeltrainer.explore_param_space import (ParameterSpace, ExData, 
+    Sample, PDskSample)  #Data structure & Sampling method
+from metamodeltrainer.models import (HyperParameters, RecModel, improve,
+    load_model)           #Neural Network management
 from metamodeltrainer.run_simulation import label
 
 
-
 cwd = Path(__file__).resolve().parents[1]
-save_path = Path(cwd,f"models_force_HBE_05_16_active_{datetime.today().strftime('%Y%m%d')}")
+save_path = Path(cwd,
+    f"models_force_HBE_05_16_active_{datetime.today().strftime('%Y%m%d')}")
 if not save_path.is_dir():
-    save_path.mkdir()
+    save_path.mkdir()   
 
 '''
 STEP 1 : Define a parameter space to explore
@@ -81,7 +83,7 @@ label_fn = lambda S: label(S,
     stress=False)
 
 S = PDskSample(PSpace, k = 4) # k indicates the number of points to sample
-X_T, Y_T = label_fn(S)
+
 
 '''
 STEP 3 : Build and train a model equipped with an uncertainty indicator on this sampling.
@@ -96,9 +98,21 @@ HP = HyperParameters(layers=[64,64],        #Architecture of the network
                      dropout_rate=0.5,      #Allows uncertainty
                      interpolation=1000)    #Faster computing
 
+X_T, Y_T = label_fn(S)
 model = RecModel(X_T,Y_T,HP)
+train_cpu_start  = time.process_time()
+train_wall_start = time.perf_counter()
 model.train(n_epochs=100,verbose=1)
-model.save(save_path / "model_base",overwrite=True)
+train_cpu  = time.process_time()  - train_cpu_start
+train_wall = time.perf_counter() - train_wall_start
+
+model_path = save_path / "model_base"
+model.save(model_path,overwrite=True)
+
+with open(model_path/'times.txt','w',encoding='utf-8') as f:
+    f.write(f"cpu_time_train,{train_cpu}\n"
+            f"wall_time_train,{train_wall}\n"
+            )
 
 '''
 STEPS 4 & 5 : Use active learning to find new points to add to the training sample and train a new model on it.
@@ -115,9 +129,29 @@ It is good practice, although not necessary, to save the model at each iteration
 '''
 
 for i in range(99):
+    model_path = save_path / f"model_improved_{str(i).zfill(3)}"
+    improve_cpu_start = improve_wall_start = 0.
+    train_cpu_start   = train_wall_start = 0.
+
+    improve_cpu_start  = time.process_time()
+    improve_wall_start = time.perf_counter()
     model_bis = improve(model,label_fn,PSpace,k=4)
+    improve_cpu  = time.process_time() - improve_cpu_start
+    improve_wall = time.perf_counter() - improve_wall_start
+
+    train_cpu_start  = time.process_time()
+    train_wall_start = time.perf_counter()
     model_bis.train(100,1)
-    model_bis.save(save_path / f"model_improved_{str(i).zfill(3)}",overwrite=True)
+    train_cpu  = time.process_time() - train_cpu_start
+    train_wall = time.perf_counter() - train_wall_start
+
+    model_bis.save(model_path,overwrite=True)
+    with open(model_path / 'times.txt','w',encoding='utf-8') as f:
+        f.write(f"cpu_time_train,{train_cpu}\n"
+                f"wall_time_train,{train_wall}\n"
+                f"cpu_time_improve {improve_cpu}:\n"
+                f"wall_time_improve: {improve_wall}\n"
+                )
     model = model_bis
 
 '''
